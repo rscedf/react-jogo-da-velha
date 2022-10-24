1a1b623e52d21f6e2367e7a1fd86f3ac46e31da4
Rafael Cardoso <rscedf@gmail.com>
2022-10-24 16:11:02 -0300

    criado componente game

---
+++ src/componets/gameOption/GameOption.jsx
@@ -0,0 +1,11 @@
+import styles from './GameOptions.module.css'
+
+function GameOption(){
+    return(
+        <div className={styles.gameOptions}>
+            
+        </div>
+    )
+}
+
+export default GameOption
--- src/componets/gamePage/GamePage.jsx
+++ src/componets/gamePage/GamePage.jsx
@@ -1,9 +1,11 @@
 import Header from '../Header/Header'
+import Game from '../game/Game'
 
 function GamePage(){
    return (
         <div className="container">
             <Header />
+            <Game />
         </div>
     )
 }
