675f70aeb7da40fa9c8899352ae4a6502a97ba9a
Rafael Cardoso <rscedf@gmail.com>
2022-10-24 20:15:34 -0300

    criado estados no gameOption

--- src/componets/game/Game.jsx
+++ src/componets/game/Game.jsx
@@ -1,19 +1,32 @@
+/*
+import useState  =  importa função de array
+import styles = importa css
+import GameOption = importa o status x ou 0
+
+const[gameState, setGameState] = cria constante do array 9 posições com valor 0
+getState.map = percorre posições array
+key = variável como única
+
+*/
+
+
+import {useState} from 'react'
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
 
 function Game(){
+    const[gameState, setGameState]=useState(Array(9).fill(0))
     return(
         <div className={styles.game}>
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
+            {
+                gameState.map((value, pos)=>
+                    <GameOption 
+                    key={'game-option-pos-${pos}'}
+                    status = {value}
+                    />
+                )
+            }
         </div>
         
     )
--- src/componets/gameOption/GameOption.jsx
+++ src/componets/gameOption/GameOption.jsx
@@ -1,9 +1,19 @@
 import styles from './GameOptions.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption(){
+const GameIcon = ({iconName})=> <Icon iconName={iconName} size = "25px" />
+
+function GameOption({status}){
     return(
         <div className={styles.gameOptions}>
-            
+            {
+                status === 1 && <GameIcon iconName="circle" />
+            }
+
+            {
+                status === -1 && <GameIcon iconName="x" />
+            }
+          
         </div>
     )
 }
