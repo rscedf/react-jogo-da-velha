ed7919d44bee578090fc904fcd07db75347220ab
Rafael Cardoso <rscedf@gmail.com>
2022-10-24 22:06:35 -0300

    criado lógica game

--- src/componets/game/Game.jsx
+++ src/componets/game/Game.jsx
@@ -3,31 +3,68 @@ import useState  =  importa função de array
 import styles = importa css
 import GameOption = importa o status x ou 0
 
-const[gameState, setGameState] = cria constante do array 9 posições com valor 0
-getState.map = percorre posições array
-key = variável como única
-
 */
 
 
 import {useState} from 'react'
 import styles from './Game.module.css'
 
+import Icon from '../icon/Icon'
 import GameOption from '../gameOption/GameOption'
 
+/* 
+const[gameState, setGameState] = cria constante do array 9 posições com valor 0 
+const[currentPlayer, setCurrentPlayer]= useState(1)  = inicia com jogador 1    0
+if verifica se posição está vazia
+variavel newGameState assume novo clic na posição
+setGameState(newGameState)  = setGame recebe o valor de newGameState
+ */
 function Game(){
     const[gameState, setGameState]=useState(Array(9).fill(0))
+    const[currentPlayer, setCurrentPlayer]= useState(1)
+
+    const handleClic = (pos)=>{
+        if(gameState[pos] === 0){
+            let newGameState = [...gameState]
+            newGameState[pos]= currentPlayer
+            setCurrentPlayer(currentPlayer * -1)
+            setGameState(newGameState)
+        }
+        
+   }
+   /*  
+    Primeira div 
+    cria tabuleiro
+    getState.map = percorre posições array
+    key = variável como única
+    Segunda div
+    cria escritas do lado tabuleiro
+   */
     return(
-        <div className={styles.game}>
+        <div className={styles.gameContent}>
+            <div className={styles.game}>
             {
                 gameState.map((value, pos)=>
                     <GameOption 
                     key={'game-option-pos-${pos}'}
                     status = {value}
+                    onClick={()=> handleClic(pos)}
                     />
                 )
             }
+             </div>
+
+             <div className={styles.gameInfo}>
+                <h4>Próximo a jogar:</h4>
+                {
+                    currentPlayer === 1 && <Icon iconName="circle" />
+                }
+                {
+                    currentPlayer === -1 && <Icon iconName="x" />
+                }
+             </div>
         </div>
+        
         
     )
 }
--- src/componets/gameOption/GameOption.jsx
+++ src/componets/gameOption/GameOption.jsx
@@ -3,9 +3,9 @@ import Icon from '../icon/Icon'
 
 const GameIcon = ({iconName})=> <Icon iconName={iconName} size = "25px" />
 
-function GameOption({status}){
+function GameOption({status, onClick}){
     return(
-        <div className={styles.gameOptions}>
+        <div className={styles.gameOptions} onClick={onClick}>
             {
                 status === 1 && <GameIcon iconName="circle" />
             }
